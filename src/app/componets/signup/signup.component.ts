e27fa8abff5509d3ecf8851e24c72e1e8fff6cd6
Rima76903 <achourryma971@gmail.com>
2025-02-23 15:16:16 +0100

    integration

--- src/app/componets/signup/signup.component.ts
+++ src/app/componets/signup/signup.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import { response } from 'express';
+import { error } from 'console';
 
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [ RouterLink,
+    CommonModule,        // Pour *ngIf, *ngFor, etc.
+    ReactiveFormsModule
+
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
-
+export class SignupComponent implements OnInit{
+  signupForm! : FormGroup;
+  successMessage: string ='';
+  errorMessage: string ='';
+  constructor(private fb:FormBuilder,private authService:AuthService){}
+  ngOnInit(): void {
+      this.signupForm = this.fb.group({
+        username:['',[Validators.required]],
+        email:['',[Validators.required]],
+        password:['',[Validators.required,Validators.minLength(6)]],
+      });
+  }
+  
+onSubmit(): void{
+  if(this.signupForm.valid){
+    this.authService.signup(this.signupForm.value).subscribe({
+      next:(response)=>{
+        this.successMessage = '✅ Utilisateur ajouté avec succès !';
+        this.errorMessage = '';
+        this.signupForm.reset(); // Réinitialiser le formulaire après succès
+      },
+      error:(err)=>{
+        this.errorMessage = '❌ Une erreur est survenue. Veuillez réessayer.';
+        this.successMessage = '';
+      },
+      
+    })
+  }
+}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl  = 'http://localhost:8080';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+    // Méthode signup
+    signup(user: any) {
+      return this.http.post(`${this.baseUrl}/auth/signup`, user); // Correct URL
+    }
+    
 }
+
